101352d278cc72f2d011964f16879e9e37fa5bff
Dorota Karkut <dorakarkut1@wp.pl>
2021-05-09 09:22:50 +0200

    Cleaning SLIC

--- SLIC.py
+++ SLIC.py
@@ -7,8 +7,6 @@ from skimage.future.graph import rag_mean_color, cut_threshold
 
 
 
-
-
 def get_image(file_path):
     """
     Reads an image and returns it with its LAB color space version
@@ -31,13 +29,6 @@ def get_image(file_path):
     image = img_as_ubyte(img)
     #image = color.bgr2rgb(image)
     return image
-
-
-def show_img(img):
-    width = 8.0
-    height = img.shape[0]*width/img.shape[1]
-    f = plt.figure(figsize=(width, height))
-    plt.imshow(img/255)
     
 def create_superpixel(image,amount,image_name):
     """
@@ -55,33 +46,52 @@ def create_superpixel(image,amount,image_name):
     labels :  the segmentation labeling of the image after all iterations
 
     """
-
-
     path = os.path.join(os.getcwd())
     os.makedirs(os.path.join(path, "data/" + image_name + "_SLIC"), exist_ok=True)
     number_of_iterations = 1
-    for i in range(1,5):
+
+    for i in range(1,4):
         number_of_iterations = number_of_iterations*i
         labels = slic(image, n_segments=amount, max_iter=number_of_iterations, enforce_connectivity=True,  slic_zero=False,start_label=1)
         image_with_boundaries = mark_boundaries(image, labels, (0, 0, 0))
-
         image_with_boundaries = img_as_ubyte(image_with_boundaries)
         io.imsave("data/" + image_name + "_SLIC/" + str(number_of_iterations) +"_SLIC.jpg", image_with_boundaries)
     
     labels = slic(image, n_segments=amount, max_iter=200, enforce_connectivity=True,  slic_zero=False,start_label=1)
     rag = rag_mean_color(image, labels)
     final_labels = cut_threshold(labels, rag,25)
-    plt.figure()
-    plt.imshow(final_labels, cmap='gray'), plt.axis('off')
-    final_label_rgb = color.label2rgb(final_labels, image, kind='avg', bg_label=0)
-    plt.figure()
-    plt.imshow(final_label_rgb), plt.axis('off')
+    final_label_average = color.label2rgb(final_labels, image, kind='avg', bg_label=0)
     image_with_boundaries = mark_boundaries(image, labels, (0, 0, 0))
     plt.figure()
     plt.imshow(image_with_boundaries, cmap='gray'), plt.axis('off')
-    io.imsave("data/" + image_name + "_SLIC/" + image_name+"_final5.jpg", final_label_rgb)
+    plt.show()
+    io.imsave("data/" + image_name + "_SLIC/" + image_name +"_final5.jpg", final_label_average)
     io.imsave("data/" + image_name + "_SLIC/" +"200_SLIC.jpg", image_with_boundaries)
+    return final_label_average
     
+
+def post_processing(I,file_path):
+    
+    se = np.ones((7,7), dtype='uint8') 
+    #close = cv2.inRange(I, 0, 255)
+    I = 255-I
+    Img = I > 0
+
+    close = cv2.threshold(I, 40, 255, cv2.THRESH_BINARY)[1]
+    #opens = cv2.morphologyEx(close, cv2.MORPH_OPEN, se)
+    #close = cv2.morphologyEx(opens, cv2.MORPH_CLOSE, se)
+    #close = cv2.morphologyEx(opens, cv2.MORPH_CLOSE, se)
+
+    #clean = remove_small_objects(Img,min_size= 10000)
+    image_name = file_path.split(".")[0]
+
+    close_bitwise = 255-close
+    
+    cv2.imwrite(image_name+ "2.jpg", close_bitwise)
+
+    plt.subplot(1, 2, 2), plt.imshow(close_bitwise, cmap='gray'), plt.title('Result-bitwise'), plt.axis("off")
+    plt.subplot(1, 2, 1), plt.imshow(close, cmap='gray'), plt.title('Result'), plt.axis("off")
+    plt.show()
 
 def main(file_path,amount_superpixel):
     
@@ -97,7 +107,7 @@ def main(file_path,amount_superpixel):
     """
     image_name = file_path.split(".")[0]
     image = get_image(file_path)
-    create_superpixel(image, amount_superpixel,image_name)
+    segmented = create_superpixel(image, amount_superpixel, image_name)
     
 
     
